5f51833b22effbab3412a069e6d6efc685b167dd
Elena Pujante Martínez <89639906+elenapuj@users.noreply.github.com>
2021-11-13 21:51:30 +0100

    Add files via upload

--- Source/Ising_L20.cpp
+++ Source/Ising_L20.cpp
@@ -1,50 +1,79 @@
 #include "Ising.hpp"
 
-#include "Ising.hpp"
 
 int main() {
 
+	//First of all, we create a file to store the data we are going to obtain
+
 	ofstream ofile1;
-	ofile1.open("Ising_L20_unordered.txt");
+	ofile1.open("Ising_L20_T1_ordered.txt");  //That is an example name, but with this file we have generated four different files
 	ofile1 << scientific;
 
-	int L = 20;
-	double T = 1;
+
+
+
+	//Then, we introduce the characteristics of the system
+
+	int L = 20;  //Length of the lattice
+	double T = 1.0;  //Temperature. This is something you can change for generating different data
+
+
+	//Set up for generating random numbers
+
 	random_device rd;
 	mt19937 mt(rd());
 
+
+	//Finally we create our system
+
 	Ising my_system(T, L, mt);
 
-	mat S(L, L);
+
+
+
+	//Now, we create some elements that we will fill up later
+
+	mat S(L, L);  //Matrix representing the lattice
+
 	int k, l;
 
-	my_system.create_matrix(S, true);
+	double e_sum, e_mean, m_sum, m_mean;
+        e_sum = 0;
+	e_mean = 0;
+	m_sum = 0;
+	m_mean = 0;
 
-	/*
-	S.print("S=");
-	my_system.flip_spin(S, k, l);
-	S.print("New S=");
-	cout << "Position of the fliped spin =" << k << l << endl;
-	double e = my_system.energy_spin(S);
-	cout << "e =" << e << endl;
-	cout << my_system.magnetization_spin(S) << endl;
-	*/
 
-	int MC_cycles = 100;
+	//We fill S with a random configuration (if true) or with all the spins being up (if false)
 
-	double e, m, e_mean, m_mean;
+	my_system.create_matrix(S, false);
+
+
+
+
+	//Once we have done this previous settings, let's start with the MCMC method. For that, we first set the number of MCMC cycles that we want to perform.
+
+	int MC_cycles = 1000;
 
 	for (int i = 0; i < MC_cycles; i++) {
 
+		//We perform a cycle of MCMC
+
 		my_system.MCMC(S, k, l);
 
-		e =+ my_system.energy_spin(S);
+
+		//Then we sum the energy and magnetization per spin of the new state to the one from the states we've sampled before that and do the mean
+
+		e_sum += my_system.energy_spin(S);
 		e_mean = e / (1.0*i+1);
 
-		m =+ abs(my_system.magnetization_spin(S));
+		m_sum += abs(my_system.magnetization_spin(S));
 		m_mean = m / (1.0*i+1);
 
-		ofile1 << e_mean << m_mean << endl;
+
+		//Finally, we introduce the new mean of each thing to the file opened before
+
+		ofile1 << e_mean << "   " << m_mean << endl;
 
 	}
 
