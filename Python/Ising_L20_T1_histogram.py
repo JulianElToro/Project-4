ac22961035f4363cc50dfeb6b3c7bb9bdb94d958
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-11-14 19:28:51 +0100

    Add files via upload

--- Python/Ising_L20_T1_histogram.py
+++ Python/Ising_L20_T1_histogram.py
@@ -12,7 +12,6 @@ with  open("Ising_L20_T1_histogram.txt", "r") as  infile:
         vals = line.split()
         average_energy.append(float(vals [0]))
         
-
 #Plot        
 
 n=1000 
@@ -22,12 +21,12 @@ mu = np.mean(average_energy)
 sigma = np.std(average_energy)
 
 
-e_values = np.linspace( mu - 4*sigma , mu + 4*sigma , n ) 
+e_values = np.linspace( mu - 3*sigma , mu + 3*sigma , n ) 
 
 p_e = (1/(sigma * np.sqrt(2*np.pi))) * (np.exp(-(e_values - mu)**2 / (2 * sigma**2) ))
     
 plt.figure()
-plt.hist(average_energy , density = True , rwidth = 0.9 ,color = "b" , label = "Histogram of ⟨ϵ⟩")
+plt.hist(average_energy , bins = [-2.03, -2.01, -1.99 , -1.97 , -1.95 ], density = True , rwidth = 0.9 ,color = "b" , label = "Histogram of ⟨ϵ⟩")
 plt.plot(e_values , p_e , color = "red", label = r'$p_ϵ(ϵ;T)$')
 plt.title(r'Histogram of ⟨ϵ⟩ for $T=1 \ J/k_B$', fontsize=10)
 plt.xlabel("⟨ϵ⟩" )
--- Python/Ising_L20_T2_histogram.py
+++ Python/Ising_L20_T2_histogram.py
@@ -24,15 +24,18 @@ sigma = np.std(average_energy)
 e_values = np.linspace( mu - 4*sigma , mu + 4*sigma , n ) 
 
 p_e = (1/(sigma * np.sqrt(2*np.pi))) * (np.exp(-(e_values - mu)**2 / (2 * sigma**2) ))
-    
+
+l_bins = list( np.linspace( - 1.8 , -0.9 , 91) )
+
 plt.figure()
-plt.hist(average_energy , density = True , rwidth = 0.9 ,color = "b" , label = "Histogram of ⟨ϵ⟩")
+plt.hist(average_energy , bins = l_bins ,density = True , rwidth = 0.85 ,color = "b" , label = "Histogram of ⟨ϵ⟩")
 plt.plot(e_values , p_e , color = "red", label = r'$p_ϵ(ϵ;T)$')
 plt.title(r'Histogram of ⟨ϵ⟩ for $T=2.4 \ J/k_B$', fontsize=10)
+plt.xlim(-1.8,-1)
 plt.xlabel("⟨ϵ⟩" )
 plt.ylabel("Probability density")
-plt.legend()
 plt.grid(axis='y' , color = "black" , linewidth = 0.7 )
+plt.legend()
 #plt.show()
 
 #The graph is saved in a PDF file
