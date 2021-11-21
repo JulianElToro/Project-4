4f82c5587bc93f74bf8161f7153dc8b31b7b6ed9
Elena Pujante Martínez <89639906+elenapuj@users.noreply.github.com>
2021-11-21 18:13:26 +0100

    Add files via upload

--- Source/Ising_L20_histogram.cpp
+++ Source/Ising_L20_histogram.cpp
@@ -5,9 +5,9 @@ int main() {
 
 	//First of all, we create a file to store the data we are going to obtain
 
-	ofstream ofile1;
-	ofile1.open("Ising_L20_T1_histogram.txt");
-	ofile1 << scientific;
+	ofstream ofile;
+	ofile.open("Ising_L20_T1_histogram.txt");  //That is an example name, but with this program we have generated four different files
+	ofile << scientific;
 
 
 
@@ -15,7 +15,7 @@ int main() {
 	//Then, we introduce the characteristics of the system
 
 	int L = 20;  //Length of the lattice
-	double N = static_cast<double>(L) * static_cast<double>(L);
+	double N_size = 1.0 * (L * L);  //Number of spins in the lattice
 	double T = 1.0;  //Temperature. This is something you can change for generating different data
 
 
@@ -27,7 +27,7 @@ int main() {
 
 	//Finally we create our system
 
-	Ising my_system(T, L, N, mt);
+	Ising my_system(T, L, N_size, mt);
 
 
 
@@ -38,16 +38,12 @@ int main() {
 
 	int k, l;
 
-	double q, dE, dM;
+	double e_value, q, dE, dM;
 
 
 	//We fill S with a random configuration (if true) or with all the spins being up (if false)
 
 	my_system.create_matrix(S, true);
-
-	e_ = my_system.energy_spin(S);
-
-        m_ = my_system.magnetization_spin(S);
 
 
 
@@ -55,7 +51,7 @@ int main() {
 	//Once we have done this previous settings, let's start with the MCMC method. For that, we first set the number of MCMC cycles that we want to perform.
 
 	int MC_cycles = 1000000;
-	int n = 15000;  //Burn-in time
+	int n = 100000;  //Burn-in time
 
 	for (int i = 0; i < MC_cycles; i++) {
 
@@ -64,20 +60,31 @@ int main() {
 		my_system.MCMC(S, k, l, q, dE, dM);
 
 
-		if (i >= n){
+		//We start storing the data only after the burn_in time
 
-			//We store the energy and the magnetization per spin of the current state
+		if (i == n){
 
-			ofile1 << (q * (dE / N)) << endl;
+			//Here we calculate the energy of the first step after the burn-in time and store it
+
+			e_value = my_system.energy_spin(S);
+
+			ofile << e_value << endl;
+
+		}
+
+		if (i > n){
+
+			//Here we just update the energy value and we store it
+
+			e_value += q * (dE / N_size);
+
+			ofile << e_value << endl;
 
 		}
 
 	}
 
-
-	//Finally, we obtain the mean of both quantities and store them in another file
-
-	ofile1.close();
+	ofile.close();
 
 	return 0;
 
