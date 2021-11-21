ffb9c61c9396b63f91c9b3e618fb47f2cb52d05a
Elena Pujante Martínez <89639906+elenapuj@users.noreply.github.com>
2021-11-21 13:53:27 +0100

    Add files via upload

--- Source/Ising_temperature_study.cpp
+++ Source/Ising_temperature_study.cpp
@@ -5,21 +5,22 @@ int main() {
 
 	//First of all, we create a file to store the data we are going to obtain
 
-	ofstream ofile1;
-	ofile1.open("Ising_L40_temperature_study.txt");  //That is an example name, but with this file we have generated four different files
-	ofile1 << scientific;
+	ofstream ofile;
+	ofile.open("Ising_L60_ts.txt");  //That is an example name, but with this file we have generated four different files
+	ofile << scientific;
 
 
 	//Some integers that we'll need several times
 
-	int MC_cycles = 100000;
-	int N = 10;
-        int n = 15000;
+	int MC_cycles = 2000000;  //Number of MCMC cycles
+	int N = 100;  //Number of temperatures that we will study
+        int n = 100000;  //Burn in time(we will take into account the energies and magnetizations from this step)
 
 
 	//Then, we introduce the characteristics of the system
 
-	int L = 40;  //Length of the lattice
+	int L = 60;  //Length of the lattice
+	double N_size = 1.0 * (L * L);  //Number of elements inside the matrix that represents the lattice
 	vec T = linspace(2.1, 2.4, N);  //Vector of temperatures
 
 
@@ -35,28 +36,25 @@ int main() {
 
         int k, l;
 
-        double e_sum, e2_sum, e_mean, e2_mean, m_sum, m2_sum, m_mean, m2_mean, Cv, X;
+        double e_, m_, e_mean, e2_mean, m_mean, m2_mean, Cv, X, q, dE, dM;
 
 
-
+	//Then, we start the loop over the temperatures
 
 	for(int j = 0; j < N; j++){
 
-		e_sum = 0;
-        	e2_sum = 0;
-		e_mean = 0;
-		e2_mean = 0;
-        	m_sum = 0;
-        	m2_sum = 0;
-		m_mean = 0;
-		m2_mean = 0;
-		Cv = 0;
-		X = 0;
+		e_mean = 0.0;
+		e2_mean = 0.0;
+		m_mean = 0.0;
+		m2_mean = 0.0;
+		Cv = 0.0;
+		X = 0.0;
+		dM = 0.0;
 
 
-		//Finally we create our system
+		//First, we create our system for the temperature of this step
 
-		Ising my_system(T(j), L, mt);
+		Ising my_system(T(j), L, N_size, mt);
 
 
 		//We fill S with a random configuration (if true) or with all the spins being up (if false)
@@ -70,31 +68,54 @@ int main() {
 
 			//We perform a cycle of MCMC
 
-			my_system.MCMC(S, k, l);
+			my_system.MCMC(S, k, l, q, dE, dM);
 
 
-			if (i >= n){
+			//We calulate here the energy for the first step after the burn-in time
+
+			if (i == n){
+
+				e_ = my_system.energy_spin(S);
+
+				m_ = my_system.magnetization_spin(S);
+
+				e_mean = e_;
+
+				m_mean = abs(m_);
+
+				e2_mean = (e_ * e_);
+
+				m2_mean = (m_ * m_);
+
+			}
 
 
-				//Then we sum the energy and magnetization per spin of the new state to the one from the states we've sampled before that and do the mean
 
-				e_sum += my_system.energy_spin(S);
-				e2_sum += (my_system.energy_spin(S)) * (my_system.energy_spin(S));
+			//After that, we update the energy and the magnetization in every step and we accumulate them to do the mean at the end
 
-				m_sum += abs(my_system.magnetization_spin(S));
-				m2_sum += (my_system.magnetization_spin(S)) * (my_system.magnetization_spin(S));
+			if (i > n){
 
+				e_ += q * (dE / N_size);  //e of this step
+				m_ += q * (dM / N_size);  //m of this step
+
+				e_mean += e_;  //Sum of all the energies from the step n
+				m_mean += abs(m_);  //Sum of all the magnetizations from the step n
+
+				e2_mean += (e_ * e_);  //Same for e²
+				m2_mean += (m_ * m_);  //Same for m²
 
 			}
 
 		}
 
 
-		e_mean = e_sum / (MC_cycles - n);
-		e2_mean = e2_sum / (MC_cycles - n);
+		//Finally, we calculate the means, Cv, X and store everything
 
-		m_mean = m_sum / (MC_cycles - n);
-                m2_mean = m2_sum / (MC_cycles - n);
+		e_mean = e_mean / (MC_cycles - n);
+		m_mean = m_mean / (MC_cycles - n);
+
+		e2_mean = e2_mean / (MC_cycles - n);
+                m2_mean = m2_mean / (MC_cycles - n);
 
 		Cv = my_system.Cv(e_mean, e2_mean);
 
@@ -103,11 +124,11 @@ int main() {
 
 		//Finally, we introduce all this results to the file opened before
 
-        	ofile1 << T(j) << "   " << e_mean << "   " << m_mean << "   " << Cv << "   " << X << endl;
+        	ofile << T(j) << "   " << e_mean << "   " << m_mean << "   " << Cv << "   " << X << endl;
 
 	}
 
-	ofile1.close();
+	ofile.close();
 
 	return 0;
 
