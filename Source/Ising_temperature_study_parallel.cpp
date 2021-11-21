16ee6a11a1aa631ffddac9150271cfac52a930e2
Elena Pujante Martínez <89639906+elenapuj@users.noreply.github.com>
2021-11-21 12:33:42 +0100

    Add files via upload

--- Source/Ising_temperature_study_parallel.cpp
+++ Source/Ising_temperature_study_parallel.cpp
@@ -13,13 +13,13 @@ int main() {
 
 	int MC_cycles = 1000000;  //Number of MCMC cycles
 	int N = 100;  //Number of temperatures in which we divide the interval of temperature
-	int n = 15000;  //Number of data that we neglect from the beggining(due to burn-in time)
-	double N_size = 1.0 * L * L;
+	int n = 100000;  //Number of data that we neglect from the beggining(due to burn-in time)
 
 
-//Then, we introduce the characteristics of the system
+	//Then, we introduce the characteristics of the system
 
 	int L = 40;  //Length of the lattice
+	double N_size = 1.0 * L * L;
 	vec T = linspace(2.1, 2.4, N);  //Vector of temperatures
 
 
@@ -29,16 +29,21 @@ int main() {
 	mt19937 mt(rd());
 
 
-	//Common name to all the files
+	/*//Common name to all the files
 
-	const string output_file_name = "Ising_tsp.txt";
+	const string output_file_name = "Ising_tsp.txt";*/
+
+
+	ofstream ofile;
+        ofile.open("Ising_L40_tsp.txt");
+        ofile << scientific;
 
 
 
 
 	//Here we start the parallelize area
 
-#pragma omp parallel
+	#pragma omp parallel
 	{
 
 		//Now, we define some elements that we will fill out later
@@ -48,9 +53,9 @@ int main() {
 		int k, l;
 
 		double e_, m_, e_mean, e2_mean, m_mean, m2_mean, Cv_value, X_value, q, dE, dM;
-		
 
-		// Prepare for file output
+
+		/*// Prepare for file output
 
 		static int print_prec = 10;  //Number of decimal positions.(???)
 
@@ -60,13 +65,13 @@ int main() {
 		ofstream ofile;
 		string my_output_file_name = output_file_name + ".thread_" + to_string(my_thread);
 		ofile.open(my_output_file_name.c_str(), ofstream::trunc);  // ofstream::trunc makes sure old content is deleted
-		ofile << setprecision(print_prec) << scientific;
+		ofile << setprecision(print_prec) << scientific;*/
 
-		
+
 
 		//Now we start the paralellized loop over j
 
-#pragma omp for
+		#pragma omp for ordered
 
 		for (int j = 0; j < N; j++) {
 
@@ -79,15 +84,15 @@ int main() {
 			Cv_value = 0.0;
 			X_value = 0.0;
 			dM = 0.0;
-						
+
 			//We create our system
 
-			Ising my_system(T(j), L, mt);
+			Ising my_system(T(j), L, N_size, mt);
 
 
 			//We fill S with a random configuration (if true) or with all the spins being up (if false)
 
-			my_system.create_matrix(S, true);
+			my_system.create_matrix(S, false);
 
 			//Once we have done this previous settings, let's start with the MCMC method
 
@@ -121,8 +126,6 @@ int main() {
 
 				if (i > n) {
 
-					double dM = S(k, l) * 2.0 / N_size;
-
 					//Then we sum the energy and magnetization per spin of the new state to the one from the states we've sampled before that(and also their squares)
 
 					e_ += q * (dE / N_size); //e of this step
@@ -144,13 +147,13 @@ int main() {
 
 			//Finally, we calculate the means, Cv, X and store everything
 
-			e_mean = e_mean / MC_cycles;
+			e_mean = e_mean / (MC_cycles - n);
 
-			m_mean = m_mean / MC_cycles;
+			m_mean = m_mean / (MC_cycles - n);
 
-			e2_mean = e2_mean / MC_cycles;
+			e2_mean = e2_mean / (MC_cycles - n);
 
-			m2_mean = m2_mean / MC_cycles;
+			m2_mean = m2_mean / (MC_cycles - n);
 
 			Cv_value = my_system.Cv(e_mean, e2_mean);
 
@@ -160,12 +163,15 @@ int main() {
 
 			//Finally, we introduce all this results to the file opened before
 
-			ofile << T(j) << "   " << e_mean << "   " << m_mean << "   " << Cv_value << "   " << X_value << endl;
+			#pragma omp ordered
 
+			ofile << T(j) << "   " << e_mean << "   " << m_mean << "   " << Cv_value << "   " << X_value << endl;
 
 		}
 
 	}
+
+	ofile.close();
 
 	return 0;
 
