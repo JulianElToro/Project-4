f7a9e10a2221fa72dd2e79dfafac24bbc5c8b57e
mariaperez01 <89605350+mariaperez01@users.noreply.github.com>
2021-11-22 19:06:29 +0100

    Update Ising_L20_T1_histogram.py

--- Python/Ising_L20_T1_histogram.py
+++ Python/Ising_L20_T1_histogram.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import List
 
-#The data gets extracted from a .txt file, in this case, the values of ϵ for each spin state
+""""
+We extract the data from an .txt file and create a vector that contains the values of ϵ for each spin state
+""""
 
 with  open("Ising_L20_T1_histogram.txt", "r") as  infile:
     
@@ -32,6 +34,7 @@ plt.figure()
 
 #The histogram with the values centered in each bin
 plt.hist(average_energy , bins = [-2.03, -2.01, -1.99 , -1.97 , -1.95 ], density = True , rwidth = 0.9 ,color = "b" , label = "Histogram of ⟨ϵ⟩")
+
 #Plot of the probability distribution of ϵ 
 plt.plot(e_values , p_e , color = "red", label = r'$p_ϵ(ϵ;T)$')
 plt.title(r'Histogram of ⟨ϵ⟩ for $T=1 \ J/k_B$', fontsize=10)
@@ -39,7 +42,6 @@ plt.xlabel("⟨ϵ⟩/J" )
 plt.ylabel("Probability density/J$^{-1}$")
 plt.legend() #Legend gets painted
 plt.grid(axis='y' , color = "black" , linewidth = 0.7 ) #Grid of horizontal lines
-#plt.show()
 
 #The graph is saved in a PDF file
 plt.savefig("Ising_L20_T1_histogram.pdf")
