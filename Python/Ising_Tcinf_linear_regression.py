76023fce9e25c38ff8affbb598bbc54d4f63170a
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-11-22 01:36:50 +0100

    Add files via upload

--- Python/Ising_Tcinf_linear_regression.py
+++ Python/Ising_Tcinf_linear_regression.py
@@ -2,6 +2,90 @@
 from scipy import stats
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import List
+
+with  open("Ising_L40_tsp(4000000).txt", "r") as  infile:
+
+    lines = infile.readlines()
+
+    T: List[float] = []
+    eps_40: List[float] = []
+    m_40: List[float] = []
+    C_v_40: List[float] = []
+    Chi_40: List[float] = []
+
+    for  line in  lines:
+        vals = line.split()
+        T.append(float(vals[0]))
+        eps_40.append(float(vals[1]))
+        m_40.append(float(vals[2]))
+        C_v_40.append(float(vals[3]))
+        Chi_40.append(float(vals[4]))
+
+with  open("Ising_L60_tsp(4000000).txt", "r") as  infile:
+
+    lines = infile.readlines()
+
+
+    eps_60: List[float] = []
+    m_60: List[float] = []
+    C_v_60: List[float] = []
+    Chi_60: List[float] = []
+
+    for  line in  lines:
+        vals = line.split()
+        eps_60.append(float(vals[1]))
+        m_60.append(float(vals[2]))
+        C_v_60.append(float(vals[3]))
+        Chi_60.append(float(vals[4]))
+
+with  open("Ising_L80_tsp(4000000).txt", "r") as  infile:
+
+    lines = infile.readlines()
+
+    T_: List[float] = []    
+    eps_80: List[float] = []
+    m_80: List[float] = []
+    C_v_80: List[float] = []
+    Chi_80: List[float] = []
+
+    for  line in  lines:
+        vals = line.split()
+        T_.append(float(vals[0]))
+        eps_80.append(float(vals[1]))
+        m_80.append(float(vals[2]))
+        C_v_80.append(float(vals[3]))
+        Chi_80.append(float(vals[4]))
+
+with  open("Ising_L100_tsp(4000000).txt", "r") as  infile:
+
+    lines = infile.readlines()
+
+    eps_100: List[float] = []
+    m_100: List[float] = []
+    C_v_100: List[float] = []
+    Chi_100: List[float] = []
+
+    for  line in  lines:
+        vals = line.split()
+        eps_100.append(float(vals[1]))
+        m_100.append(float(vals[2]))
+        C_v_100.append(float(vals[3]))
+        Chi_100.append(float(vals[4]))
+
+# Temperatures where the Cv is maximum
+
+Tc1 = T[np.argmax(C_v_40)]
+Tc2 = T[np.argmax(C_v_60)]
+Tc3 = T[np.argmax(C_v_80)]
+Tc4 = T[np.argmax(C_v_100)]
+
+# Temperatures where the X is maximum
+
+Tc1_b = T[np.argmax(Chi_40)]
+Tc2_b = T[np.argmax(Chi_60)]
+Tc3_b = T[np.argmax(Chi_80)]
+Tc4_b = T[np.argmax(Chi_100)]
 
 # Size of the matrix
 
@@ -11,11 +95,13 @@ Linv = 1/L
 
 # Resullts for the critical temperature for different sizes
 
-#TcL = np.array[("cositas")]
+TcL = np.array([Tc1, Tc2 , Tc3 , Tc4])
 
-TcL = np.random.random(4)+2
+TcL_b = np.array([Tc1_b, Tc2_b , Tc3_b , Tc4_b])
 
-# Linear regression
+#TcL = np.random.random(4)+2
+
+# Linear regression with Cv
 
 slope, intercept, r_value, p_value, std_err = stats.linregress(Linv,TcL)
 
@@ -31,12 +117,41 @@ plt.xlabel(r'$L^-1$')
 
 plt.grid()
 
-plt.text(0.019, 2.7, '$T_c(L) - T_c(L=\infty) = aL^{-1}$')
+plt.text(0.019, 2.28, '$T_c(L) - T_c(L=\infty) = aL^{-1}$')
 
-plt.text( 0.019, 2.67, 'a = '+ str(round(slope,2)) + '  $J/k_B$')
+plt.text( 0.019, 2.275, 'a = '+ str(round(slope,2)) + '  $J/k_B$')
 
-plt.text(0.019, 2.64, r'$T_c(L = \infty) \ = $' + str(round(intercept,2)) + '  $J/k_B$')
+plt.text(0.019, 2.270, r'$T_c(L = \infty) \ = $' + str(round(intercept,2)) + '  $J/k_B$')
 
-plt.text( 0.019 , 2.61, r'$R^2$ = ' + str(round(r_value,2)) )
+plt.text( 0.019 ,2.265, r'$R^2$ = ' + str(round(-r_value,2)) )
 
-plt.title( r'Linear regression of $T_c(L)$ and $L^{-1}$ ')
+plt.title( r'Linear regression of $T_c(L)$ and $L^{-1}$ ($C_V$ results) ')
+
+plt.show()
+
+
+# Linear regression with Cv
+
+slope_b, intercept_b, r_value_b, p_value_b, std_err_B = stats.linregress(Linv,TcL_b)
+
+plt.plot( Linv , TcL_b , 'o', color = 'blue')
+
+plt.plot( Llinspace , slope_b*Llinspace + intercept_b, color = 'red')
+
+plt.ylabel(r'$T_c(L) / \ J \ k_B^{-1}$')
+
+plt.xlabel(r'$L^-1$')
+
+plt.grid()
+
+plt.text(0.019, 2.36, '$T_c(L) - T_c(L=\infty) = aL^{-1}$')
+
+plt.text( 0.019, 2.34, 'a = '+ str(round(slope_b,2)) + '  $J/k_B$')
+
+plt.text(0.019, 2.32, r'$T_c(L = \infty) \ = $' + str(round(intercept_b,2)) + '  $J/k_B$')
+
+plt.text( 0.019 ,2.30, r'$R^2$ = ' + str(round(-r_value_b,2)) )
+
+plt.title( r'Linear regression of $T_c(L)$ and $L^{-1}$ ($\chi$ results) ')
+
+plt.show()
